762a3ce7ed0a4f9a345c609c85e8da88ab469fd2
Heitor Godau <heitorgodau@gmail.com>
2019-09-14 21:35:06 -0300

    cors implemented

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 // DB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,10 +14,10 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 const app = express();
 
 // CORS
-// app.use(cors({
-//   credentials: true,
-//   origin: ['http://localhost:3000'], // <== this will be the URL of React app (it will be running on port 3000)
-// }));
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'], // <== this will be the URL of React app (it will be running on port 3000)
+}));
 
 app.use(bodyParser.json());
 
